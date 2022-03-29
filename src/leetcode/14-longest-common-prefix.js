c6b2c9bdc044d005039fd8b61a7807ff32501925
JunQu <junjv@outlook.com>
2022-03-29 15:34:18 +0800

    feat: add longest common prefix

---
+++ src/leetcode/14-longest-common-prefix.js
@@ -0,0 +1,19 @@
+const longestCommonPrefix = (strs) => {
+  let ret = strs[0] || ''
+  for(const str of strs ) {
+    let tmpStr = ''
+    for(let i=0; i < str.length || i < ret.length; i++) {
+      if(str[i] === ret[i]) {
+        tmpStr += str[i]
+      } else {
+        ret = tmpStr
+        break
+      }
+    }
+    if(!ret) {
+      return ret
+    }
+  }
+  return ret
+};
+
--- src/leetcode/README.md
+++ src/leetcode/README.md
@@ -1,3 +1,4 @@
 # Leetcode 练习记录
 
 [9 palindrome number](./9-isPalindrome.js) 3-28
+[14 palindrome number](./14-longest-common-prefix.js) 3-29
