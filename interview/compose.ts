fa4e04b9f6941bd4d75ff236384e3133ceb55a07
JunQu <junjv@outlook.com>
2022-06-30 09:32:51 +0800

    feat: add compose

--- interview/compose.ts
+++ interview/compose.ts
@@ -1,34 +1,100 @@
-// const arr = [
-//   (next: Function) => {
-//     console.log(1)
-//     next()
-//     console.log(1.1)
-//   },
-//   (next: Function) => {
-//     console.log(2)
-//     next()
-//     console.log(2.1)
-//   },
-//   (next: Function) => {
-//     console.log(3)
-//     next()
-//     console.log(3.1)
-//   },
-// ]
-//
-// export const compose = (arr: ((next: Function) => void)[]) => {
-//   const fns = [...arr]
-//   const recursion = () => {
-//     while (fns.length) {
-//       const fn = fns.shift()!
-//       fn(recursion)
-//     }
-//   }
-//   return recursion
-// }
-//
-// const t1 = performance.now()
-// const fn = compose(arr)
+type Middleware = (next: Function) => void
+
+const middleware: Middleware[] = [
+  (next) => {
+    console.log(1)
+    next()
+    console.log(1.1)
+  },
+  (next) => {
+    console.log(2)
+    next()
+    console.log(2.2)
+  },
+]
+
+function manualCompose() {
+  const middleware: Middleware[] = [
+    (next) => {
+      console.log(1)
+      next()
+      console.log(1.1)
+    },
+    (next) => {
+      console.log(2)
+      next()
+      console.log(2.2)
+    },
+  ]
+
+  const firstFn = middleware[0]
+  const lastFn = middleware[1]
+
+  const lastNext = () => {}
+
+  const next = () => {
+    lastFn(lastNext)
+  }
+
+  firstFn(next)
+}
+
+// manualCompose()
+
+function esCompose([fn, ...fns]: Middleware[]) {
+  return () => fn?.(esCompose(fns))
+}
+
+const fn = esCompose(middleware)
+
 // fn()
-// const t2 = performance.now()
-// console.log(t2 - t1)
+
+const reduceRight = middleware.reduceRight(
+  (next, fn) => {
+    return () => fn(next)
+  },
+  () => {}
+)
+
+// reduceRight()
+
+const recursiveCompose = (middleware: Middleware[] = [], current = 0) => {
+  if (current === middleware.length) {
+    return () => {}
+  }
+  const next = current + 1
+  return () => middleware[current](recursiveCompose(middleware, next))
+}
+
+const fs = recursiveCompose(middleware)
+// fs()
+
+const reduce = function (middleware: Function[] = []) {
+  return middleware.reduce((current, next) => {
+    return (arg: any) => current(next(arg))
+  })
+}
+
+const reducerArr = [(a: number) => a + 1, (a: number) => a * 2, (a: number) => a + 3]
+
+// console.log(reduce(ss)(5))
+
+const reduceCompose = function (middleware: Middleware[] = []) {
+  return middleware.reduce((current, next) => {
+    return (arg) => current(() => next(arg))
+  })(() => {})
+}
+
+// reduceCompose(middleware)
+
+const asyncCompose = (middleware: Middleware[]) => {
+  return Promise.resolve(
+    // @ts-ignore
+    middleware.reduceRight(
+      (next, fn) => () => Promise.resolve(fn(next)),
+      () => Promise.resolve()
+    )()
+  )
+}
+
+asyncCompose(middleware)
