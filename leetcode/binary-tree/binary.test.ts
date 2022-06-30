67d32d4f6aee3e893b3d962412a2f7909b2168bc
JunQu <junjv@outlook.com>
2022-06-30 09:47:42 +0800

    feat: add 101 symmetric tree

--- leetcode/binary-tree/binary.test.ts
+++ leetcode/binary-tree/binary.test.ts
@@ -1,6 +1,7 @@
 import { inorderTraversal } from './94-binary-tree-inorder-traversal'
 import { it, expect, describe } from 'vitest'
 import { arrToTree } from '../helper/binary'
+import { isSymmetric } from './101-symmetric-tree'
 
 describe('二叉树的基本遍历', () => {
   it('前序遍历', () => {})
@@ -17,4 +18,24 @@ describe('二叉树的基本遍历', () => {
   it('后序遍历', () => {})
 
   it('层序遍历', () => {})
+})
+
+describe('二叉树的比较', () => {
+  it.only('101 对称二叉树', () => {
+    const tree1 = arrToTree([1])
+    const tree2 = arrToTree([1, 2, 2, 3, 4, 4, 3])
+    const tree3 = arrToTree([1, 2, 2, null, 3, 3, null])
+
+    const tree4 = arrToTree([1, 2, 2, null, 3, null, 3])
+    const tree5 = arrToTree([1, 2, 3])
+    const tree6 = arrToTree([1, 2, 3, null, 3, null])
+
+    expect(isSymmetric(tree1)).toBe(true)
+    expect(isSymmetric(tree2)).toBe(true)
+    expect(isSymmetric(tree3)).toBe(true)
+
+    expect(isSymmetric(tree4)).toBe(false)
+    expect(isSymmetric(tree5)).toBe(false)
+    expect(isSymmetric(tree6)).toBe(false)
+  })
 })
